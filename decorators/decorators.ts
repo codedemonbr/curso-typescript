09490195855ced7948d45aee7d2f8c81f3f7699f
Henrique <henrique.thsantos.ths@gmail.com>
2021-06-08 14:12:21 -0300

    Decorator factory

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,14 +1,29 @@
-@logarClasse
+// @logarClasse
+// @logarClasseSe(false)
+@decorator({ a: "Teste" })
 class Eletrodomestico {
     constructor() {
         console.log("novo...");
     }
 }
+/**
+ * Decorator só é chamado quando a classe é carregada
+ */
 
 function logarClasse(construtor: Function) {
     console.log(construtor);
 }
-
 /**
- * Decorator só é chamado quando a classe é carregada
+ * recebe nada e retorna nada _: Function é usado apenas para manter a assinatura
  */
+function decoratorVazio(_: Function) {}
+
+function logarClasseSe(valor: boolean) {
+    return valor ? logarClasse : decoratorVazio;
+}
+
+function decorator(obj: { a: string; b?: number }) {
+    return function (_: Function): void {
+        console.log(obj.a + " " + obj.b);
+    };
+}
